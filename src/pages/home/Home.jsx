32501d5f49f8163da8208e895aed9cf6b1c3ae31
Rostislav Parahnenko <asawww100@gmail.com>
2022-12-14 02:07:02 +0200

    12121

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
 
 import './home.css'
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <>
     <MainHeader/>
+    <Programs/>
     </>
   )
 }
