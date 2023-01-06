33f2c972575294bd17ed2a7a5c3478c1eb162981
Rostislav Parahnenko <asawww100@gmail.com>
2023-01-06 23:33:55 +0200

    2023

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,9 @@
+import FAQs from '../../components/FAQs'
+import Footer from '../../components/Footer'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
+import Testimonials from '../../components/Testimonials'
+import Values from '../../components/Values'
 
 import './home.css'
 
@@ -9,8 +13,15 @@ const Home = () => {
     <>
     <MainHeader/>
     <Programs/>
+    <Values />
+    <FAQs />
+    <Testimonials />
+    <Footer />
     </>
   )
 }
 
 export default Home
+
+
+
